2d743ae6e5b3bb465bdf660a11ba4b848daf1f82
mohamedhany1231 <mohamedhany61@gmail.com>
2025-09-25 18:54:03 +0300

    change loader to use the logo

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -3,16 +3,12 @@ import React from 'react';
 export default function Loader() {
   return (
     <div className='relative inset-0 flex flex-col items-center justify-center w-full h-full '>
-      <div className='p-8 flex flex-col items-center justify-center space-y-4'>
-        {/* <img
-          className='w-32 h-32  absolute top-[25%] animate-pulse'
-          src={'/lovable-uploads/6b90bd6a-9b4a-4cfe-89f0-5a06cd21ab9a.png'}
+      <div className='p-8 flex flex-col  items-center justify-center space-y-4 animate-pulse'>
+        <img
+          className='min-h16 max-h-[50%] aspect-square '
+          src={'/logo/2.svg'}
           alt='Loading icon'
-        /> */}
-        {/* TODO: use logo instead of text */}
-        <p className=' text-2xl font-bold text-white animate-pulse'>
-          loading...
-        </p>
+        />
       </div>
     </div>
   );
